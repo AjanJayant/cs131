c7dcfcfdd7af80f8e069e4c1fcd0b0491d64f955
Ajan Jayant <ajanj@ucla.edu>
2013-01-16 08:26:57 -0800

    1/16/2013

--- Assignment1/hw1.ml
+++ Assignment1/hw1.ml
@@ -21,13 +21,29 @@ let get_start g =
 let filter_blind_alleys g = 
 	(get_start g, remove_rules (get_start g) snd(g) [] []);;
 
-let rec check_RHS rhs terminal_sym = 
-	match rhs with 
+let rec check_RHS rhs terminal_sym =
+	match rhs with
 	|[] -> true
-	|h::t -> match h with 
-		  | (N a) -> (match subset [h] terminal_sym with
-			      | false -> false
-			      | true ->check_RHS t terminal_sym)
-                  | (T a) -> check_RHS t terminal_sym;;
+	|h::t -> match h with
+		 |(N a) -> (match subset [h] terminal_sym with
+			   |false -> false
+		           |true ->check_RHS t terminal_sym)
+                 |(T a) -> check_RHS t terminal_sym;;
 
+let check_rule rule term_sym = 
+  	match rule with
+  	|(a,b) -> match (check_RHS b term_sym) with
+  		  |true -> true
+  		  |false -> false;;
 
+let rec get_terminals rules = 
+	match rules with 
+	|[] -> []
+	|h::t -> get_terminals t @ [T (fst(h))];;
+
+let rec mark_terminal orig_list curr_list term_list = 
+	match curr_list with
+	|[] -> term_list
+	|h::t -> match check_rule h (get_terminals term_list) with
+		 |true -> mark_terminal orig_list orig_list (term_list @ [h])
+		 |false -> mark_terminal orig_list t term_list;;
